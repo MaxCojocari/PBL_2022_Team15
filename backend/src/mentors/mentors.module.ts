1213afae7715592f1687383dc2b8471f96a57ede
MaxCojocari <cojocarigoncearmax@gmail.com>
2022-11-05 14:20:20 +0200

    update code style

--- backend/src/mentors/mentors.module.ts
+++ backend/src/mentors/mentors.module.ts
@@ -1,15 +1,14 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { MentorSchema } from "./mentor.model";
-
 import { MentorsController } from "./mentors.controller";
 import { MentorsService } from "./mentors.service";
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([{name: 'Mentor', schema: MentorSchema}])
-    ],
-    controllers: [MentorsController],
-    providers: [MentorsService]
+  imports: [
+    MongooseModule.forFeature([{ name: 'Mentor', schema: MentorSchema }])
+  ],
+  controllers: [MentorsController],
+  providers: [MentorsService]
 })
-export class MentorsModule{}
+export class MentorsModule { }
--- backend/src/mentors/mentors.service.ts
+++ backend/src/mentors/mentors.service.ts
@@ -1,104 +1,103 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-
 import { Mentor } from "./mentor.model";
 
 @Injectable()
-export class MentorsService{
+export class MentorsService {
 
-    constructor(@InjectModel('Mentor') private readonly mentorModel: Model<Mentor>){
-    };
+  constructor(@InjectModel('Mentor') private readonly mentorModel: Model<Mentor>) {
+  };
 
-    async insertMentor(fN: string, lN: string, email: string, linkedin: string, twitter: string, ind: string, acc: string){
-        const newMentor = new this.mentorModel({
-            firstName: fN,
-            lastName: lN,
-            email,
-            linkedinURL: linkedin,
-            twitterURL: twitter,
-            industries: ind,
-            accelerators: acc
-        });
+  async insertMentor(fN: string, lN: string, email: string, linkedin: string, twitter: string, ind: string, acc: string) {
+    const newMentor = new this.mentorModel({
+      firstName: fN,
+      lastName: lN,
+      email,
+      linkedinURL: linkedin,
+      twitterURL: twitter,
+      industries: ind,
+      accelerators: acc
+    });
 
-        const result = await newMentor.save();
-        return result.id as string;
+    const result = await newMentor.save();
+    return result.id as string;
+  }
+
+  async getMentors() {
+    const mentors = await this.mentorModel.find().exec();
+    return mentors as Mentor[];
+  }
+
+  async getSingleMentor(mentorId: string) {
+    const mentor = await this.findMentor(mentorId);
+    return mentor;
+  }
+
+  async modifyMentor(
+    mentorId: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    linkedinURL: string,
+    twitterURL: string,
+    industries: string[],
+    accelerators: string[]
+  ) {
+    const updatedMentor = await this.findMentor(mentorId);
+
+    if (firstName) {
+      updatedMentor.firstName = firstName;
     }
 
-    async getMentors(){
-        const mentors = await this.mentorModel.find().exec();
-        return mentors as Mentor[];
+    if (lastName) {
+      updatedMentor.lastName = lastName;
     }
 
-    async getSingleMentor(mentorId: string){
-        const mentor = await this.findMentor(mentorId);
-        return mentor;
+    if (email) {
+      updatedMentor.email = email;
     }
 
-    async modifyMentor(
-        mentorId: string,
-        firstName: string,
-        lastName: string,
-        email: string,
-        linkedinURL: string,
-        twitterURL: string,
-        industries: string[],
-        accelerators: string[]
-      ) {
-        const updatedMentor = await this.findMentor(mentorId);
-        
-        if(firstName){
-            updatedMentor.firstName = firstName;
-        } 
-
-        if(lastName){
-            updatedMentor.lastName = lastName;
-        }
-
-        if(email){
-            updatedMentor.email = email;
-        }
-
-        if(linkedinURL){
-            updatedMentor.linkedinURL = linkedinURL;
-        }
-
-        if(twitterURL){
-            updatedMentor.twitterURL = twitterURL;
-        }
-
-        if(industries){
-            updatedMentor.industries = industries;
-        }
-
-        if(accelerators){
-            updatedMentor.accelerators = accelerators;
-        }
-
-        updatedMentor.save();
-      }
-
-    async deleteMentor(mentorId: string) {
-        const result = await this.mentorModel.deleteOne({_id: mentorId}).exec();
-        if (result.deletedCount === 0) {
-          throw new NotFoundException('Could not find mentor.');
-        }
-
-        return result;
+    if (linkedinURL) {
+      updatedMentor.linkedinURL = linkedinURL;
     }
 
-    private async findMentor(id: string): Promise<Mentor>{
-        let mentor;
-        try {
-            mentor = await this.mentorModel.findById(id);
-        } catch (error) {
-            throw new NotFoundException('Could not find mentor');
-        }
-
-        if(!mentor){
-            throw new NotFoundException('Could not find mentor');
-        }
-
-        return mentor;
+    if (twitterURL) {
+      updatedMentor.twitterURL = twitterURL;
     }
+
+    if (industries) {
+      updatedMentor.industries = industries;
+    }
+
+    if (accelerators) {
+      updatedMentor.accelerators = accelerators;
+    }
+
+    updatedMentor.save();
+  }
+
+  async deleteMentor(mentorId: string) {
+    const result = await this.mentorModel.deleteOne({ _id: mentorId }).exec();
+    if (result.deletedCount === 0) {
+      throw new NotFoundException('Could not find mentor.');
+    }
+
+    return result;
+  }
+
+  private async findMentor(id: string): Promise<Mentor> {
+    let mentor;
+    try {
+      mentor = await this.mentorModel.findById(id);
+    } catch (error) {
+      throw new NotFoundException('Could not find mentor');
+    }
+
+    if (!mentor) {
+      throw new NotFoundException('Could not find mentor');
+    }
+
+    return mentor;
+  }
 }
